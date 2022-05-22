24c309f1c99e9f10104bf4521301dd1657869104
Eric-Fulla <ericfull007@gmail.com>
2022-05-22 16:57:50 +0200

    nou grounded raycasts

--- Assets/Scripts/GroundDetector.cs
+++ Assets/Scripts/GroundDetector.cs
@@ -7,11 +7,18 @@ public class GroundDetector : MonoBehaviour
     public bool isGrounded = true;
     public float distance = 0.3f;
     public LayerMask collisionLayerMask;
+    public float offset = 0.2f;
 
     private void FixedUpdate()
     {
-        Ray l_Ray = new Ray(transform.position, Vector3.down);
-        if (Physics.Raycast(l_Ray, distance + 0.01f, collisionLayerMask.value))
+        Ray l_Ray1 = new Ray(transform.position + new Vector3 (offset, 0, 0), Vector3.down);
+        Ray l_Ray2 = new Ray(transform.position + new Vector3(- offset, 0, 0), Vector3.down);
+        Ray l_Ray3 = new Ray(transform.position + new Vector3(0, 0, offset), Vector3.down);
+        Ray l_Ray4 = new Ray(transform.position + new Vector3(0, 0, - offset), Vector3.down);
+        if (Physics.Raycast(l_Ray1, distance + 0.01f, collisionLayerMask.value) ||
+            Physics.Raycast(l_Ray2, distance + 0.01f, collisionLayerMask.value) ||
+            Physics.Raycast(l_Ray3, distance + 0.01f, collisionLayerMask.value) ||
+            Physics.Raycast(l_Ray4, distance + 0.01f, collisionLayerMask.value))
         {
             isGrounded = true;
         }
